a05eeded440a080dd3f3e898ad43b83617cb86e0
Aayush Thapa <aayushthapa7@gmail.com>
2020-03-30 22:41:02 +0545

    use constraints for calcluating child height based on parent

--- lib/widgets/chat_bar.dart
+++ lib/widgets/chat_bar.dart
@@ -9,40 +9,48 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        FittedBox(child: Text('\$${spendAmount.toStringAsFixed(0)}')),
-        SizedBox(
-          height: 4,
-        ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.grey),
-                    color: Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(10)),
+    return LayoutBuilder(
+      builder: (ctx, constraints) {
+        return Column(
+          children: <Widget>[
+            Container(
+                height: constraints.maxHeight * 0.15,
+                child: FittedBox(
+                    child: Text('\$${spendAmount.toStringAsFixed(0)}'))),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+              height: constraints.maxHeight * 0.6,
+              width: 10,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(width: 1, color: Colors.grey),
+                        color: Color.fromRGBO(220, 220, 220, 1),
+                        borderRadius: BorderRadius.circular(10)),
+                  ),
+                  FractionallySizedBox(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.grey),
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                    heightFactor: spendPrcAmount,
+                  ),
+                ],
               ),
-              FractionallySizedBox(
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.grey),
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(10)),
-                ),
-                heightFactor: spendPrcAmount,
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          height: 4,
-        ),
-        Text('$label')
-      ],
+            ),
+            SizedBox(
+              height: constraints.maxHeight * 0.05,
+            ),
+            Container(
+                height: constraints.maxHeight * 0.15, child: Text('$label'))
+          ],
+        );
+      },
     );
   }
 }
